6375d0e69d3fb7f3201ca5ec6b53809dc6b94902
ekta <ektajhaaec@gmail.com>
2025-05-05 18:11:49 +0530

    python1 day3 code

---
+++ skillcaptain/python1/day3.py
@@ -0,0 +1,21 @@
+# You are tasked with designing a Python program, define a two numbers num1 and num2 and execute the following operations:
+# 1. Calculate and display the sum of the two numbers.
+# 2. Determine and show the result of subtracting the second number from the first.
+# 3. Compute and print the product of the two numbers.
+# 4. Find the quotient of dividing the first number by the second and output the result.
+
+# Make sure your program handles unexpected situations well and gives clear and simple output for every operation.
+
+# Output:
+# Let's suppose the first number: 8 and the second number: 3
+# 1. Sum of the two numbers: 11
+# 2. Result of subtraction: 5
+# 3. Product of the two numbers: 24
+# 4. Quotient of division: 2.6666666666666665
+
+num1 =1808
+num2 = 17
+print(f"Sum of the two numbers:{num1 + num2}")
+print(f"Result of subtraction:{num1 - num2}")
+print(f"Product of the two numbers:{num1 * num2}")
+print(f"Quotient of division:{num1 / num2}")
