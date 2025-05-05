b3886bee43acda4e859cb145acdf5dbfc00ce320
ekta <ektajhaaec@gmail.com>
2025-05-05 15:07:01 +0530

    Python1Day1

---
+++ skillcaptain/python1/day1.py
@@ -0,0 +1,5 @@
+#Write a basic Python program where a variable is defined to represent your favorite Pokemon, and the program prints a message saying it. 
+
+
+name = "pikachu"
+print(f"my favourite pokimon is {name}" )
