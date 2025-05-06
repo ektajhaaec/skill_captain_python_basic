782b0b663321f5b0fb8e08f7cbb523a9ab503980
ekta <ektajhaaec@gmail.com>
2025-05-06 16:37:13 +0530

    is_even function

---
+++ skillcaptain/python1/day7.py
@@ -0,0 +1,9 @@
+#Define a Python function named is_even that takes an integer parameter and returns True if the provided number is even; otherwise, it returns False.
+
+def is_even(num):
+    if num%2 ==0 :
+        return True
+    else:
+        return False
+    
+print(is_even(46))
