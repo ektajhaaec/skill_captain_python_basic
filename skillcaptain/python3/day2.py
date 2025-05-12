5e6204f24ff579d3f968ebe35c056085f4c03b41
ekta <ektajhaaec@gmail.com>
2025-05-12 23:30:55 +0530

    calling module in another module

---
+++ skillcaptain/python3/day2.py
@@ -0,0 +1,10 @@
+from math_operations import MathOperations
+
+
+a ,b = 2,10
+maths_operationday2 = MathOperations()
+
+print(maths_operationday2.add(a,b))
+print(maths_operationday2.subs(a,b))
+print(maths_operationday2.mul(a,b))
+print(maths_operationday2.power(a,b))
---
+++ skillcaptain/python3/math_operations.py
@@ -0,0 +1,11 @@
+class MathOperations :
+    def add (self,a,b):
+        return (a+b)
+    def subs(sel,a,b):
+        return (b-a)
+    def mul(self,a,b):
+        return (a*b)
+    def divide(self,a,b):
+        return (b/a)
+    def power(self,a,b):
+        return (a**b)
